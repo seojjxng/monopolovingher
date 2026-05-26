30fb714099455db521fc83a86509b083e6c97beb
seojjxng <nero4886@gmail.com>
2026-05-26 11:15:57 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyBV8Kcd8mF2CGlIRWDxDR6B3f1_50V3FH0",
+    authDomain: "monopolovely.firebaseapp.com",
+    databaseURL: "https://monopolovely-default-rtdb.firebaseio.com",
+    projectId: "monopolovely",
+    storageBucket: "monopolovely.firebasestorage.app",
+    messagingSenderId: "325639499908",
+    appId: "1:325639499908:web:94ad73a6d82da77ce552d1",
+    measurementId: "G-GDKYQYLFQJ"
+}; 
+
